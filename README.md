ea890e4d6e1435b9c396baaca633a16439d8cad1
Sam Atman <atmanistan@gmail.com>
2024-09-05 10:09:10 -0400

    Begin runerip
    A port of Björn Hjörman's UTF-8 DFA transducer.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Runerip
+
+For great **Justice**!
---
+++ src/runerip.zig
@@ -0,0 +1,6 @@
+// Zig source file for runerip
+const std = @import("std");
+
+test "module mentioned" {
+    std.debug.print("hello from module\n", .{});
+}
