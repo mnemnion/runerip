52c0529a9ac35a29fe14160fcadc8fae0fda2620
Sam Atman <atmanistan@gmail.com>
2024-09-05 10:56:20 -0400

    Translate the basic format
    Not much else to this library to be frank

--- src/runerip.zig
+++ src/runerip.zig
@@ -1,6 +1,84 @@
 // Zig source file for runerip
 const std = @import("std");
 
-test "module mentioned" {
-    std.debug.print("hello from module\n", .{});
+// zig fmt: off
+
+/// Byte transitions: value to class
+const u8dfa: [256]u8 = .{
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 00..1f
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 20..3f
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 40..5f
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 60..7f
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9, // 80..9f
+7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7, // a0..bf
+8,8,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, // c0..df
+0xa,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x3,0x4,0x3,0x3, // e0..ef
+0xb,0x6,0x6,0x6,0x5,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8,0x8, // f0..ff
+0x0,0x1,0x2,0x3,0x5,0x8,0x7,0x1,0x1,0x1,0x4,0x6,0x1,0x1,0x1,0x1, // s0..s0
+1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,1, // s1..s2
+1,2,1,1,1,1,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1, // s3..s4
+1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,3,1,3,1,1,1,1,1,1, // s5..s6
+1,3,1,1,1,1,1,3,1,3,1,1,1,1,1,1,1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1, // s7..s8
+};
+
+/// State transition: state + class = new state
+const st_dfa: [108]u8 = .{
+ 0,12,24,36,60,96,84,12,12,12,48,72,
+12,12,12,12,12,12,12,12,12,12,12,12,
+12, 0,12,12,12,12,12, 0,12, 0,12,12,
+12,24,12,12,12,12,12,24,12,24,12,12,
+12,12,12,12,12,12,12,24,12,12,12,12,
+12,24,12,12,12,12,12,12,12,24,12,12,
+12,12,12,12,12,12,12,36,12,36,12,12,
+12,36,12,12,12,12,12,36,12,36,12,12,
+12,36,12,12,12,12,12,12,12,12,12,12,
+};
+
+// zig fmt: on
+
+// Probably need mask offset for variants e.g. C0 and C1 rejecting DFA
+
+/// State masks (experiment, shifts may be faster)
+const c_mask: [12]u8 = .{
+    0,
+    0,
+    0b0011_1111,
+    0b0001_1111,
+    0b0000_1111,
+    0b0000_0111,
+    0b0000_0011,
+    0,
+    0,
+    0,
+    0,
+    0,
+};
+
+/// Successful codepoint parse
+pub const RUNE_ACCEPT = 0;
+
+/// Error state
+pub const RUNE_REJECT = 12;
+
+pub inline fn decodeNext(state: *u32, rune: *u32, byte: u8) u32 {
+    const class: u32 = u8dfa[byte];
+    rune.* = if (state.* != RUNE_ACCEPT)
+        (byte & 0x3f) | (rune.* << 6)
+    else
+        (byte & c_mask[class]);
+    state.* = st_dfa[state.* + class];
+    return state.*;
+}
+
+/// Decode one byte.  Returns `true` if a full rune is decoded, `false`
+/// if not, and throws error{InvalidUnicode} if an invalid  state is
+/// reached.
+pub inline fn decoded(state: *u32, rune: *u32, byte: u8) !bool {
+    const accept = decodeNext(state, rune, byte);
+    if (accept == RUNE_ACCEPT)
+        return true
+    else if (accept == RUNE_REJECT)
+        return error.InvalidUnicode
+    else
+        return false;
 }
